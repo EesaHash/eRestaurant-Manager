dd43203244957b3844dd1fb2191bd8837cf55156
= <=>
2021-09-06 14:17:35 +1000

    added new project

---
+++ src/main/java/com/restaurant/model/Person.java
@@ -0,0 +1,20 @@
+package com.restaurant.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @ToString
+public class Person implements Serializable {
+    @Id
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String role;
+    private String phone;
+}
