cfbc25a6d266f7463e63a54d8091edbf7b1d11eb
Eesa Hash <eesa6000@gmail.com>
2021-09-06 14:43:20 +1000

    replaced RestController with standard Controller, added empty person service class

---
+++ src/main/java/com/restaurant/service/PersonService.java
@@ -0,0 +1,13 @@
+package com.restaurant.service;
+
+import com.restaurant.repository.PersonRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+}
