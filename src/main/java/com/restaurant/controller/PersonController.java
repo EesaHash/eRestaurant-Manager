4bdfa56868f885c0d77eee40bec5a2e643563f0c
Eesa Hash <eesa6000@gmail.com>
2021-09-06 15:32:57 +1000

    added methods to PersonService and PersonController

--- src/main/java/com/restaurant/controller/PersonController.java
+++ src/main/java/com/restaurant/controller/PersonController.java
@@ -1,8 +1,26 @@
 package com.restaurant.controller;
 
+import com.restaurant.service.PersonService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class PersonController {
 
+    private PersonService personService;
+
+    @Autowired
+    public PersonController (PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping
+    public String getPerson(Model model) {
+        model.addAttribute("something", personService.findAll());
+        return "people";
+    }
 }
--- src/main/java/com/restaurant/service/PersonService.java
+++ src/main/java/com/restaurant/service/PersonService.java
@@ -1,13 +1,38 @@
 package com.restaurant.service;
 
+import com.restaurant.model.Person;
 import com.restaurant.repository.PersonRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
+@Service @Transactional
 public class PersonService {
 
-    private PersonRepository personRepository;
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public Person addPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
+    public void deletePerson(Long id) {
+        personRepository.deleteById(id);
+    }
+
+    public Optional<Person> findPerson(Long id) {
+        return personRepository.findById(id);
+    }
 
 }
