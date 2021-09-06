f03293fadf67ff932e6f5e0d86b9279d9f736d01
= <=>
2021-09-06 14:16:05 +1000

    Deleted everything

--- README.md
+++
@@ -1,5 +0,0 @@
-# eRestaurant Application
-
-eRestaurant Booking and Ordering Application
-This repository contains two folders, client for the frontend and
-server for the backend. 
--- server/src/main/java/com/restaurant/eRestaurant/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.eRestaurant.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
