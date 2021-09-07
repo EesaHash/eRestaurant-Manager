977c4c2a8e13370bd7e6a9c27053553f9fcf5d96
Eesa Hash <eesa6000@gmail.com>
2021-09-07 13:36:45 +1000

    added test htmls for login and registration, renamed some variables in the RegistrationController

---
+++ src/main/java/com/restaurant/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package com.restaurant.controller;
+
+import com.restaurant.dto.RegistrationDto;
+import com.restaurant.service.PersonService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+
+    private final PersonService personService;
+
+    public RegistrationController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @ModelAttribute("user")
+    public RegistrationDto personRegistrationDto() {
+        return new RegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") RegistrationDto registrationDto) {
+        System.out.println(registrationDto.toString());
+        //RegistrationDto reg = new RegistrationDto("Eesa","Hash",
+       //         "eesa6000@gmail.com","test123","04232342");
+        personService.addPerson(registrationDto);
+        return "redirect:/registration?success";
+    }
+}
